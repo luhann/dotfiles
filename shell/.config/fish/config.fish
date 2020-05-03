9f19e126b75b109ceada9e687ad0ea64d4f3ee3b
Luke Hannan <luke@lukehannan.com>
2020-05-04 01:54:46 +0200

    Updated texlive to texlive 2020

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if status --is-login
-    set -x PATH ~/bin $PATH ~/.cargo/bin ~/.local/bin /home/khonsu/applications/texlive/2019/bin/x86_64-linux
-    set -x MANPATH ":/home/khonsu/applications/texlive/2019/texmf-dist/doc/man"
-    set -x INFOPATH ":/home/khonsu/applications/texlive/2019/texmf-dist/doc/info"
+    set -x PATH ~/bin $PATH ~/.cargo/bin ~/.local/bin /home/khonsu/applications/texlive/2020/bin/x86_64-linux
+    set -x MANPATH ":/home/khonsu/applications/texlive/2020/texmf-dist/doc/man"
+    set -x INFOPATH ":/home/khonsu/applications/texlive/2020/texmf-dist/doc/info"
 end
 
 set -Ux XDG_CURRENT_DESKTOP KDE
