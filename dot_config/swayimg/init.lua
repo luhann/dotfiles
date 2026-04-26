d31aeb6346548423ff3e7c413b596c67855e031c
Luke Hannan <luke@lukehannan.com>
2026-04-26 23:27:18 +0200

    Update swayimg to lua config

---
+++ dot_config/swayimg/init.lua
@@ -0,0 +1,24 @@
+swayimg.set_mode("viewer")
+swayimg.viewer.limit_preload(50)
+swayimg.imagelist.enable_adjacent(true)
+
+swayimg.on_window_resize(function()
+    swayimg.viewer.set_fix_scale("optimal")
+end)
+
+swayimg.viewer.on_image_change(function()
+    swayimg.viewer.set_default_scale("optimal")
+end)
+
+-- Keybinds
+swayimg.viewer.on_key("Left", function()
+    swayimg.viewer.switch_image("prev")
+end)
+
+swayimg.viewer.on_key("Right", function()
+    swayimg.viewer.switch_image("next")
+end)
+
+swayimg.viewer.on_key("q", function()
+  swayimg.exit()
+end)
