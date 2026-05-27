4adb55209b6d0243401a661bf3d11a67472dfac2
Luke Hannan <luke@lukehannan.com>
2026-05-27 23:20:25 +0200

    Add keybind to show image metadata

--- dot_config/swayimg/init.lua
+++ dot_config/swayimg/init.lua
@@ -2,6 +2,8 @@ swayimg.set_mode("viewer")
 swayimg.viewer.limit_preload(50)
 swayimg.imagelist.enable_adjacent(true)
 
+swayimg.gallery.enable_pstore(true)
+
 swayimg.on_window_resize(function()
   swayimg.viewer.set_fix_scale("optimal")
 end)
@@ -17,6 +19,10 @@ end)
 
 swayimg.viewer.on_key("Right", function()
   swayimg.viewer.switch_image("next")
+end)
+
+swayimg.viewer.on_key("i", function()
+    swayimg.text.show()
 end)
 
 swayimg.viewer.on_key("q", function()
